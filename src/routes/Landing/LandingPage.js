344bbf45f5f67fba451dce80b8ac43f1efdea6d5
Amber Meritt <amber.merittcampbell@gmail.com>
2019-12-12 23:00:20 -0700

    added NavBar

---
+++ src/routes/Landing/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.css'
+import NavBar from '../../components/NavBar/NavBar'
+
+export default function LandingPage(){
+    return (
+        <>
+            <NavBar />
+            <main role="main">
+            
+            </main>
+        </>
+    )
+}
