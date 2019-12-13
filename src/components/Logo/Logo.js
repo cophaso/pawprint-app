e317d8dec7299a62648f3ad34cb2910e8e825e70
Amber Meritt <amber.merittcampbell@gmail.com>
2019-12-12 23:02:09 -0700

    merged landing-page branch for navbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Logo.css'
+
+export default function Logo(){
+    return (
+        <div className='logo'>
+            <img src='https://www.pinclipart.com/picdir/big/200-2005573_swirly-heart-with-paw-prints-decal-window-sticker.png' alt='logo' />
+            <div className='logo-title'>
+                <h1>PawPrint</h1>
+                <p>Keeping up with the office pups</p>
+            </div>
+        </div>
+    )
+}
