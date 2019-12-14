c8225ebacfad8b4f06103ee64567cae28a83cfdb
Amber Meritt <amber.merittcampbell@gmail.com>
2019-12-14 13:29:04 -0700

    added UsersPupsList and UsersPups components

---
+++ src/components/UsersPup/UsersPup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './UsersPup.css'
+import Lab from '../../assets/images/lab.jpg'
+
+export default function UsersPup(){
+    return (
+        <>
+            <li>
+                <img src={Lab} alt='placeholder pup' />
+                <p>Doggo's name</p>
+            </li>
+        </>
+    )
+}
--- src/routes/UsersPups/UsersPupsPage.js
+++ src/routes/UsersPups/UsersPupsPage.js
@@ -1,35 +1,17 @@
 import React from 'react'
 import './UsersPupsPage.css'
 import NavBar from '../../components/NavBar/NavBar'
+import UsersPupsList from '../../components/UsersPupsList/UsersPupsList'
 
 export default function UsersPupsPage(){
     return (
         <>
             <NavBar />
             <main role='main'>
-            <h2>Your Pups</h2>
-            <ul>
-                <li>
-                <img src='http://critapix.com/wp-content/uploads/2019/08/dog.jpg'/>
-                <p>Doggo's name</p>
-                </li>
-                <li>
-                <img src='http://critapix.com/wp-content/uploads/2019/08/dog.jpg'/>
-                <p>Doggo's name</p>
-                </li>
-                <li>
-                <img src='http://critapix.com/wp-content/uploads/2019/08/dog.jpg'/>
-                <p>Doggo's name</p>
-                </li>
-                <li>
-                <img src='http://critapix.com/wp-content/uploads/2019/08/dog.jpg'/>
-                <p>Doggo's name</p>
-                </li>
-                <li>
-                <img src='http://critapix.com/wp-content/uploads/2019/08/dog.jpg'/>
-                <p>Doggo's name</p>
-                </li>
-            </ul>
+                <section className='usersPupsList-section'>
+                    <h2>Your Pups</h2>
+                    <UsersPupsList />
+                </section>
             </main>
         </>
     )
