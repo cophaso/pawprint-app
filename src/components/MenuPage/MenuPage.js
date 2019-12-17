e1372dc417ae3ccbcf082722340847a8adbe0024
Amber Meritt <amber.merittcampbell@gmail.com>
2019-12-16 17:31:02 -0700

    adeed MenuPage

---
+++ src/components/MenuPage/MenuPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MenuPage.css'
+
+export default function MenuPage(){
+    return(
+        <>
+            <main>
+                <ul>
+                    <li>Home</li>
+                    <li>Make a Petservation</li>
+                    <li>Your Pups</li>
+                    <li>Dog Pack</li>
+                    <li>Signout</li>
+                </ul>
+            </main>
+        </>
+    )
+}
