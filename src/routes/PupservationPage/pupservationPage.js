72e789107cb669ce094c21f41583b3abf1f7cb3d
Kayla Graham <kaylagraham@Kaylas-MBP.attlocal.net>
2019-12-23 16:37:31 -0800

    working on wiring the pupservation page

--- src/routes/PupservationPage/pupservationPage.js
+++ src/routes/PupservationPage/pupservationPage.js
@@ -6,26 +6,44 @@ import "react-datepicker/dist/react-datepicker.css";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import PupservationConfirm from '../../components/PupservationConfirm/PupservationConfirm';
-
+import config from '../../config';
 
 class PupservationPage extends React.Component {
     state = {
-      startDate: new Date()
+      startDate: new Date(),
+      pup_name: "", 
+      service_type: "",
     };
 
-    handleChange = date => {
+  // sets the date for the date picker
+  handleChange = date => {
       this.setState({
         startDate: date
       });
     };
 
-    
    
     render() {
+
+      // ADD A PUP FORM --> PROFILE ABOUT
+      // get api for pups via fetch
+      // and check the userid === parentd then show pups
+      // submit redirect to the profile page
+      // profile updated with submitted form info
+
+      // SERVICES FORM --> PROFILE SERVICES
+      // get api for pupservices via fetch
+      // use pupsservice service for services via fetch
+      // and check the userid === parentd then show pups
+      // submit redirect to the profile page
+      // profile updated with submitted form info
+
+      // options are user's pups
         const options = [
             'Buster', 'Pickles', 'Kevin'
         ];
-        const options2 = [
+        const options2 = [ 
+          // services are always the same
             'Grooming', 'Vet', 'Daycare'
         ];
           const defaultOption = options[0];
@@ -37,6 +55,9 @@ class PupservationPage extends React.Component {
         <NavBar />
         <div className="pupservationContainer">
         <h2 className='pupservation-heading'>Make a Pupservation</h2>
+        <form 
+            className='pupservationForm'
+            onSubmit={e => this.handleSubmit(e)}>
             <h3 className='pupservation-header'>Pick a date:</h3>
                 <div className='dateDropdown'>
                     <DatePicker
@@ -51,6 +72,7 @@ class PupservationPage extends React.Component {
         <Dropdown options={options2} onChange={this._onSelect} value={defaultOption2} placeholder="Select an option" />
         
         <PupservationConfirm />
+        </form>
         </div>
         </div>
       );
