a337137bbfe8a66336e988cbd62748b157b0e055
cophaso <cophaso@gmail.com>
2019-12-27 17:57:49 -0800

    hamburger menu login logout

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -8,8 +8,18 @@ export default props => {
    const handleLogoutClick = () => {
         TokenService.clearAuthToken();
     }
-    return(
-        <Menu {...props}>
+
+    const renderLogoutLink = () => {
+        return (
+            <ul>
+                <li><Link to='/login' className='bugerLink'>Login</Link></li>
+                <li><Link to='/signup' className='bugerLink'>Signup</Link></li>
+            </ul>
+        )
+      }
+    
+      const renderLoginLink = () => {
+        return (
             <ul>
                 <li><Link to='/' className='bugerLink'>Home</Link></li>
                 <li><Link to='/pupservations' className='bugerLink'>Make a Petservation</Link></li>
@@ -17,6 +27,15 @@ export default props => {
                 <li><Link to='/' className='bugerLink'>Dog Pack</Link></li>
                 <li><Link to='/' className='bugerLink' onClick={handleLogoutClick}>Signout</Link></li>
             </ul>
+        )
+      }
+
+    return(
+        <Menu {...props}>
+            {TokenService.hasAuthToken()
+                ? renderLoginLink()
+                : renderLogoutLink()
+                }
         </Menu>
     )
 }
